3b0907f2ff1f2be74e809b5024a4009c14a1dcea
Paul Wrubel <pwrubel7@gmail.com>
2023-12-13 01:55:53 -0600

    Add day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,11 @@
-use crate::{AdventError, ExclusivePart};
+use std::fs;
+
+use crate::{
+    utils::{Grid, GridEntry},
+    AdventError, ExclusivePart,
+};
+
+const INPUT_FILE: &str = "./resources/day11_input.txt";
 
 pub fn run(epart: ExclusivePart) -> Result<String, AdventError> {
     match epart {
@@ -8,9 +15,246 @@ pub fn run(epart: ExclusivePart) -> Result<String, AdventError> {
 }
 
 fn part_one() -> Result<String, AdventError> {
-    Err(AdventError::Unimplemented)
+    // read input file
+    let input = fs::read_to_string(INPUT_FILE)?;
+
+    let mut galaxy_map = GalaxyMap::parse_from_str(&input)?;
+
+    galaxy_map.set_empty_space_expansion_scalar(2);
+
+    let total_distance = galaxy_map.find_sum_of_all_galaxy_pair_distances();
+
+    Ok(total_distance.to_string())
 }
 
 fn part_two() -> Result<String, AdventError> {
-    Err(AdventError::Unimplemented)
+    // read input file
+    let input = fs::read_to_string(INPUT_FILE)?;
+
+    let mut galaxy_map = GalaxyMap::parse_from_str(&input)?;
+
+    galaxy_map.set_empty_space_expansion_scalar(1_000_000);
+
+    let total_distance = galaxy_map.find_sum_of_all_galaxy_pair_distances();
+
+    Ok(total_distance.to_string())
+}
+
+struct GalaxyMap {
+    data: Grid<Sector>,
+    empty_row_indices: Vec<usize>,
+    empty_column_indices: Vec<usize>,
+    empty_space_expansion_scalar: u64,
+}
+
+impl GalaxyMap {
+    fn parse_from_str(input: &str) -> Result<Self, String> {
+        // assumed to all be the same length!
+        let width = input.lines().next().unwrap().len();
+        let height = input.lines().count();
+
+        let mut data = Grid::new_empty(width, height);
+        for (y, line) in input.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                data.set(x as i64, y as i64, Sector::try_parse(c)?)?;
+            }
+        }
+
+        let mut empty_row_indices = Vec::new();
+        for (y, mut row) in data.rows_iter().enumerate() {
+            if row.all(|s| *s == Sector::EmptySpace) {
+                // this might hurt...
+                empty_row_indices.push(y);
+            }
+        }
+
+        let mut empty_column_indices = Vec::new();
+        for (x, mut column) in data.columns_iter().enumerate() {
+            if column.all(|s| *s == Sector::EmptySpace) {
+                // this might hurt...
+                empty_column_indices.push(x);
+            }
+        }
+
+        Ok(Self {
+            data,
+            empty_row_indices,
+            empty_column_indices,
+            empty_space_expansion_scalar: 1,
+        })
+    }
+
+    fn set_empty_space_expansion_scalar(&mut self, scalar: u64) {
+        self.empty_space_expansion_scalar = scalar;
+    }
+
+    fn _expand(&mut self) -> Result<(), String> {
+        self._expand_by_distance(1)
+    }
+
+    fn _expand_by_distance(&mut self, distance: u64) -> Result<(), String> {
+        if distance == 0 {
+            return Ok(());
+        }
+
+        let mut expanded = self.data.clone();
+
+        let mut x = 0;
+        while x < expanded.width() {
+            let built_column = expanded.build_column(x).unwrap();
+            if built_column.iter().all(|s| *s == Sector::EmptySpace) {
+                // this might hurt...
+                for _ in 0..distance {
+                    let new_column = built_column.clone();
+                    expanded.insert_column_at(x, new_column)?;
+                }
+                // this skips the columns we just inserted
+                x += distance as usize;
+            }
+            // this moves on like normal
+            x += 1;
+        }
+
+        let mut y = 0;
+        while y < expanded.height() {
+            let row = expanded.row(y).unwrap().clone();
+            if row.iter().all(|s| *s == Sector::EmptySpace) {
+                // this might hurt...
+                for _ in 0..distance {
+                    let new_row = row.clone();
+                    expanded.insert_row_at(y, new_row)?;
+                }
+                // this skips the row we just inserted
+                y += distance as usize;
+            }
+            // this moves on like normal
+            y += 1;
+        }
+
+        self.data = expanded;
+
+        Ok(())
+    }
+
+    fn find_sum_of_all_galaxy_pair_distances(&self) -> i64 {
+        self.find_galaxy_pairs()
+            .iter()
+            .map(|pair| {
+                pair.true_distance(
+                    &self.empty_row_indices,
+                    &self.empty_column_indices,
+                    self.empty_space_expansion_scalar,
+                )
+            })
+            .sum()
+    }
+
+    fn find_galaxy_pairs(&self) -> Vec<GalaxyPair> {
+        let mut galaxy_pairs = Vec::new();
+
+        let galaxy_coords = self.find_galaxy_coords();
+        for ai in 0..galaxy_coords.len() - 1 {
+            for bi in ai + 1..galaxy_coords.len() {
+                galaxy_pairs.push(GalaxyPair::new(galaxy_coords[ai], galaxy_coords[bi]))
+            }
+        }
+
+        galaxy_pairs
+    }
+
+    fn find_galaxy_coords(&self) -> Vec<(i64, i64)> {
+        self.data
+            .entries()
+            .filter_map(|GridEntry { x, y, value }| {
+                if *value == Sector::Galaxy {
+                    Some((x as i64, y as i64))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+enum Sector {
+    #[default]
+    EmptySpace,
+    Galaxy,
+}
+
+impl Sector {
+    fn try_parse(c: char) -> Result<Self, String> {
+        match c {
+            '.' => Ok(Sector::EmptySpace),
+            '#' => Ok(Sector::Galaxy),
+            _ => Err(format!("unexpected character: {}", c)),
+        }
+    }
+}
+
+impl std::fmt::Display for Sector {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Sector::EmptySpace => write!(f, "."),
+            Sector::Galaxy => write!(f, "#"),
+        }
+    }
+}
+
+struct GalaxyPair {
+    a: (i64, i64),
+    b: (i64, i64),
+}
+
+impl GalaxyPair {
+    fn new(a: (i64, i64), b: (i64, i64)) -> Self {
+        Self { a, b }
+    }
+
+    fn true_distance(
+        &self,
+        empty_row_indices: &Vec<usize>,
+        empty_column_indices: &Vec<usize>,
+        expansion_scalar: u64,
+    ) -> i64 {
+        self.true_x_distance(empty_column_indices, expansion_scalar)
+            + self.true_y_distance(empty_row_indices, expansion_scalar)
+    }
+
+    fn true_x_distance(&self, empty_column_indices: &Vec<usize>, expansion_scalar: u64) -> i64 {
+        let min_x = self.a.0.min(self.b.0) as usize;
+        let max_x = self.a.0.max(self.b.0) as usize;
+        let crossed_expanded_columns = empty_column_indices
+            .iter()
+            .filter(|x| min_x < **x && **x < max_x)
+            .count() as u64;
+
+        (max_x - min_x - crossed_expanded_columns as usize) as i64
+            + (crossed_expanded_columns * expansion_scalar) as i64
+    }
+
+    fn true_y_distance(&self, empty_row_indices: &Vec<usize>, expansion_scalar: u64) -> i64 {
+        let min_y = self.a.1.min(self.b.1) as usize;
+        let max_y = self.a.1.max(self.b.1) as usize;
+        let crossed_expanded_rows = empty_row_indices
+            .iter()
+            .filter(|y| min_y < **y && **y < max_y)
+            .count() as u64;
+
+        (max_y - min_y - crossed_expanded_rows as usize) as i64
+            + (crossed_expanded_rows * expansion_scalar) as i64
+    }
+
+    fn _manhattan_distance(&self) -> i64 {
+        self._x_distance() + self._y_distance()
+    }
+
+    fn _x_distance(&self) -> i64 {
+        (self.a.0 - self.b.0).abs()
+    }
+
+    fn _y_distance(&self) -> i64 {
+        (self.a.1 - self.b.1).abs()
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -42,3 +42,177 @@ pub fn format_duration(d: Duration) -> String {
 
     s
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Grid<T> {
+    data: Vec<Vec<T>>,
+    width: usize,
+    height: usize,
+}
+
+impl<T> Grid<T>
+where
+    T: Default + std::fmt::Debug + Clone + Copy + PartialEq,
+{
+    pub fn new_empty(width: usize, height: usize) -> Self {
+        Self {
+            data: vec![vec![T::default(); width]; height],
+            width,
+            height,
+        }
+    }
+
+    pub fn take_data(self) -> Vec<Vec<T>> {
+        self.data
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn is_within_bounds(&self, x: i64, y: i64) -> bool {
+        x >= 0 && x < self.width as i64 && y >= 0 && y < self.height as i64
+    }
+
+    pub fn get(&self, x: i64, y: i64) -> Option<T> {
+        if self.is_within_bounds(x, y) {
+            self.data
+                .get(y as usize)
+                .and_then(|row| row.get(x as usize))
+                .copied()
+        } else {
+            None
+        }
+    }
+
+    /// Get the value at (x, y) without checking if it is within bounds
+    ///
+    /// # Panics
+    ///
+    /// Panics if (x, y) is out of bounds
+    ///
+    /// I wouldn't recommend using this, but it's here anyways
+    pub fn must_get(&self, x: i64, y: i64) -> T {
+        self.data[y as usize][x as usize]
+    }
+
+    pub fn set(&mut self, x: i64, y: i64, value: T) -> Result<(), String> {
+        if self.is_within_bounds(x, y) {
+            self.data[y as usize][x as usize] = value;
+            Ok(())
+        } else {
+            Err(format!("({}, {}) is out of bounds", x, y))
+        }
+    }
+
+    pub fn row(&self, y: usize) -> Option<&Vec<T>> {
+        if y < self.height {
+            Some(&self.data[y])
+        } else {
+            None
+        }
+    }
+
+    /// Gets a column as a vector
+    ///
+    /// Due to the way the data is stored, an "artificial" column is created to hold the data
+    /// which results in the column effectively being a cloned vector
+    pub fn build_column(&self, x: usize) -> Option<Vec<T>> {
+        if x < self.width {
+            Some(self.data.iter().map(|row| row[x]).collect())
+        } else {
+            None
+        }
+    }
+
+    pub fn rows_iter(&self) -> impl Iterator<Item = impl Iterator<Item = &T>> {
+        self.data.iter().map(|row| row.iter())
+    }
+
+    pub fn columns_iter(&self) -> impl Iterator<Item = impl Iterator<Item = &T>> {
+        (0..self.width).map(move |x| (0..self.height).map(move |y| &self.data[y][x]))
+    }
+
+    pub fn insert_row_at(&mut self, y: usize, row: Vec<T>) -> Result<(), String> {
+        if row.len() != self.width {
+            return Err(format!(
+                "Row must have {} elements, but has {}",
+                self.width,
+                row.len()
+            ));
+        }
+        self.data.insert(y, row);
+        self.height += 1;
+        Ok(())
+    }
+
+    pub fn insert_column_at(&mut self, x: usize, column: Vec<T>) -> Result<(), String> {
+        if column.len() != self.height {
+            return Err(format!(
+                "Column must have {} elements, but has {}",
+                self.height,
+                column.len()
+            ));
+        }
+        for y in 0..self.height {
+            self.data[y].insert(x, column[y]);
+        }
+        self.width += 1;
+        Ok(())
+    }
+
+    pub fn entries(&self) -> impl Iterator<Item = GridEntry<&T>> {
+        self.data.iter().enumerate().flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(x, value)| GridEntry { x, y, value })
+        })
+    }
+}
+
+impl<T> std::fmt::Display for Grid<T>
+where
+    T: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (y, row) in self.data.iter().enumerate() {
+            for value in row {
+                write!(f, "{}", value)?;
+            }
+            if y < self.height - 1 {
+                writeln!(f)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<T> TryFrom<Vec<Vec<T>>> for Grid<T> {
+    type Error = String;
+
+    fn try_from(value: Vec<Vec<T>>) -> Result<Self, Self::Error> {
+        let height = value.len();
+        let width = value.get(0).map_or(0, Vec::len);
+        for row in &value {
+            if row.len() != width {
+                return Err("All rows must have the same width".to_string());
+            }
+        }
+
+        Ok(Self {
+            data: value,
+            width,
+            height,
+        })
+    }
+}
+
+pub struct GridEntry<T> {
+    pub x: usize,
+    pub y: usize,
+    pub value: T,
+}
