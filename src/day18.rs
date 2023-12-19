493375c1a204c12e3cd9b24ee5d652cf090b112b
Paul Wrubel <pwrubel7@gmail.com>
2023-12-19 00:29:09 -0600

    Add day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,4 +1,15 @@
-use crate::{AdventError, ExclusivePart};
+use std::{fs, thread::current};
+
+use itertools::Itertools;
+
+use crate::{
+    utils::{
+        CardinalDirection, Grid, GridEntry, GridPoint, OrdinalDirection, RelativeDirection, Winding,
+    },
+    AdventError, ExclusivePart,
+};
+
+const INPUT_FILE: &str = "./resources/day18_input.txt";
 
 pub fn run(epart: ExclusivePart) -> Result<String, AdventError> {
     match epart {
@@ -8,9 +19,346 @@ pub fn run(epart: ExclusivePart) -> Result<String, AdventError> {
 }
 
 fn part_one() -> Result<String, AdventError> {
-    Err(AdventError::Unimplemented)
+    // read input file
+    let input = fs::read_to_string(INPUT_FILE)?;
+
+    let dig_plan = DigPlan::parse(&input, false)?;
+
+    let mut lagoon = Lagoon::parse(&dig_plan)?;
+
+    lagoon.fill_interior()?;
+
+    let num_tiles = lagoon.get_num_dug_out_terrain_tiles();
+
+    Ok(num_tiles.to_string())
 }
 
 fn part_two() -> Result<String, AdventError> {
-    Err(AdventError::Unimplemented)
+    // read input file
+    let input = fs::read_to_string(INPUT_FILE)?;
+
+    println!("parsing plan...");
+    let dig_plan = DigPlan::parse(&input, true)?;
+
+    let num_tiles = dig_plan.shoelace_area()?;
+
+    Ok(num_tiles.to_string())
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Lagoon {
+    map: Grid<TerrainTile>,
+    trench_points: Vec<GridPoint>,
+    winding: Winding,
+}
+
+impl Lagoon {
+    fn parse(dig_plan: &DigPlan) -> Result<Self, String> {
+        let mut map = Grid::new_empty(1, 1);
+        let mut trench_points = Vec::new();
+
+        // start in the top left, in a dug out cube
+        let mut current_pos: GridPoint = (0, 0).into();
+        map.set(current_pos, TerrainTile::Trench)?;
+        trench_points.push(current_pos);
+
+        // dig out the cubes according to the plan
+        let previous_direction: Option<CardinalDirection> = None;
+        let mut left_turns = 0;
+        let mut right_turns = 0;
+        for (_i, step) in dig_plan.steps.iter().enumerate() {
+            // println!("starting step {}: {:?} as {}", i + 1, step, current_pos);
+
+            if let Some(previous_direction) = previous_direction {
+                match previous_direction.relative_direction_to(&step.direction) {
+                    RelativeDirection::Left => {
+                        left_turns += 1;
+                    }
+                    RelativeDirection::Right => {
+                        right_turns += 1;
+                    }
+                    _ => {}
+                }
+            }
+
+            for _ in 0..step.distance {
+                current_pos = current_pos.neighbor_in_direction(step.direction);
+                map.set_expand(current_pos, TerrainTile::Trench)?;
+                trench_points.push(current_pos);
+            }
+        }
+
+        let winding = if left_turns > right_turns {
+            Winding::Left
+        } else {
+            Winding::Right
+        };
+
+        Ok(Lagoon {
+            map,
+            trench_points,
+            winding,
+        })
+    }
+
+    fn fill_interior(&mut self) -> Result<(), String> {
+        for i in 0..self.trench_points.len() - 1 {
+            let current = self.trench_points[i];
+            let next = self.trench_points[i + 1];
+
+            let direction = current.direction_to_orthogonal(&next)?;
+            let offset_direction = direction.turn(self.winding.into());
+
+            let current_offset = current.neighbor_in_direction(offset_direction);
+            let next_offset = next.neighbor_in_direction(offset_direction);
+
+            let is_floodable = |t: &TerrainTile| *t == TerrainTile::Surface;
+
+            self.map
+                .flood(current_offset, &|| TerrainTile::Interior, &is_floodable)?;
+            self.map
+                .flood(next_offset, &|| TerrainTile::Interior, &is_floodable)?;
+        }
+        Ok(())
+    }
+
+    fn get_num_dug_out_terrain_tiles(&self) -> usize {
+        let mut count = 0;
+        for GridEntry { value, .. } in self.map.entries() {
+            if *value == TerrainTile::Trench || *value == TerrainTile::Interior {
+                count += 1;
+            }
+        }
+        count
+    }
+}
+
+impl std::fmt::Display for Lagoon {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let start = self.trench_points[0];
+        let direction = start
+            .direction_to_orthogonal(&self.trench_points[1])
+            .unwrap();
+        write!(
+            f,
+            "start: {}, direction: {}, winding: {}\n{}",
+            start, direction, self.winding, self.map
+        )
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+enum TerrainTile {
+    #[default]
+    Surface,
+    Trench,
+    Interior,
+}
+
+impl std::fmt::Display for TerrainTile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TerrainTile::Surface => ".",
+                TerrainTile::Trench => "#",
+                TerrainTile::Interior => "#",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct DigPlan {
+    steps: Vec<DigPlanStep>,
+    corners: Vec<GridPoint>,
+    winding: Winding,
+}
+
+impl DigPlan {
+    fn parse(input: &str, extract_from_color: bool) -> Result<Self, String> {
+        let mut steps = Vec::new();
+        let mut corners = Vec::new();
+
+        let mut current_position: GridPoint = (0, 0).into();
+        let mut previous_direction: Option<CardinalDirection> = None;
+        let mut left_turns = 0;
+        let mut right_turns = 0;
+        for line in input.lines() {
+            // parse the actual step
+            let step = DigPlanStep::parse(line, extract_from_color)?;
+
+            // push this position as a corner and update the current position
+            corners.push(current_position);
+            current_position = current_position
+                .neighbor_in_direction_distance(step.direction, step.distance as i64);
+
+            // check for winding
+            if let Some(previous_direction) = previous_direction {
+                match previous_direction.relative_direction_to(&step.direction) {
+                    RelativeDirection::Left => {
+                        left_turns += 1;
+                    }
+                    RelativeDirection::Right => {
+                        right_turns += 1;
+                    }
+                    _ => {}
+                }
+            }
+            previous_direction = Some(step.direction);
+
+            steps.push(step);
+        }
+
+        let winding = if left_turns > right_turns {
+            Winding::Left
+        } else {
+            Winding::Right
+        };
+
+        Ok(DigPlan {
+            steps,
+            corners,
+            winding,
+        })
+    }
+
+    fn true_corners(&self) -> Result<Vec<GridPoint>, String> {
+        let mut true_corners = Vec::new();
+
+        let relative_winding: RelativeDirection = self.winding.into();
+
+        for i in 0..self.corners.len() {
+            let previous_corner = if i == 0 {
+                &self.corners[self.corners.len() - 1]
+            } else {
+                &self.corners[i - 1]
+            };
+            let this_corner = &self.corners[i];
+            let next_corner = &self.corners[(i + 1) % self.corners.len()];
+
+            // direction to neighboring corners
+            // this determines what kind of corner WE are
+            let direction_backwards = this_corner.direction_to_orthogonal(previous_corner)?;
+            let direction_forwards = this_corner.direction_to_orthogonal(next_corner)?;
+
+            // this is which way we turned here.
+            let relative_direction = direction_backwards
+                .opposite()
+                .relative_direction_to(&direction_forwards);
+
+            let mut outside_corner = direction_backwards.halfway_to(&direction_forwards)?;
+
+            // if the way we turned matches the winding, it means we turned INWARDS,
+            // so we have to shift outwards to include the boundary
+            if relative_direction == relative_winding {
+                outside_corner = outside_corner.opposite();
+            }
+
+            let mut true_corner = this_corner.clone();
+            match outside_corner {
+                OrdinalDirection::NorthEast => {
+                    true_corner.x += 1;
+                    true_corner.y -= 1;
+                }
+                OrdinalDirection::SouthEast => {
+                    true_corner.x += 1;
+                }
+                OrdinalDirection::SouthWest => {}
+                OrdinalDirection::NorthWest => {
+                    true_corner.y -= 1;
+                }
+            }
+
+            true_corners.push(true_corner);
+        }
+        Ok(true_corners)
+    }
+
+    fn shoelace_area(&self) -> Result<u64, String> {
+        let corners = self.true_corners()?;
+
+        let mut area: i64 = 0;
+        for i in 0..corners.len() {
+            let j = (i + 1) % corners.len();
+
+            let a = corners[i];
+            let b = corners[j];
+
+            area += a.x * b.y - a.y * b.x;
+        }
+
+        Ok((area.abs() / 2) as u64)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct DigPlanStep {
+    direction: CardinalDirection,
+    distance: u64,
+    color: String,
+}
+
+impl DigPlanStep {
+    fn parse(input: &str, extract_from_color: bool) -> Result<Self, String> {
+        let parts = input.split_whitespace().collect_vec();
+
+        if parts.len() != 3 {
+            return Err(format!("invalid input (wrong number of parts): {}", input));
+        }
+
+        if extract_from_color {
+            Self::parse_from_color(parts)
+        } else {
+            Self::parse_from_direction_and_distance(parts)
+        }
+    }
+
+    fn parse_from_direction_and_distance(parts: Vec<&str>) -> Result<Self, String> {
+        let direction = match parts[0] {
+            "U" => CardinalDirection::North,
+            "D" => CardinalDirection::South,
+            "L" => CardinalDirection::West,
+            "R" => CardinalDirection::East,
+            _ => return Err(format!("invalid direction: {}", parts[0])),
+        };
+
+        let distance = parts[1].parse::<u64>().map_err(|err| err.to_string())?;
+
+        let color = parts[2][2..8].to_string();
+
+        Ok(DigPlanStep {
+            direction,
+            distance,
+            color,
+        })
+    }
+
+    fn parse_from_color(parts: Vec<&str>) -> Result<Self, String> {
+        // parse as hexadecimal number
+
+        let distance = u64::from_str_radix(&parts[2][2..7], 16).map_err(|err| err.to_string())?;
+
+        let direction_char = parts[2]
+            .chars()
+            .nth(7)
+            .ok_or(format!("invalid color: {}", parts[2]))?;
+
+        let direction = match direction_char {
+            '3' => CardinalDirection::North,
+            '1' => CardinalDirection::South,
+            '2' => CardinalDirection::West,
+            '0' => CardinalDirection::East,
+            _ => return Err(format!("invalid direction: {}", parts[0])),
+        };
+
+        let color = parts[2].to_string();
+
+        Ok(DigPlanStep {
+            direction,
+            distance,
+            color,
+        })
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -45,6 +45,7 @@ pub fn format_duration(d: Duration) -> String {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Grid<T> {
+    top_left: GridPoint,
     data: Vec<Vec<T>>,
     width: usize,
     height: usize,
@@ -56,6 +57,7 @@ where
 {
     pub fn new_empty(width: usize, height: usize) -> Self {
         Self {
+            top_left: GridPoint { x: 0, y: 0 },
             data: vec![vec![T::default(); width]; height],
             width,
             height,
@@ -71,6 +73,7 @@ where
             }
         }
         Ok(Self {
+            top_left: GridPoint { x: 0, y: 0 },
             data,
             width,
             height,
@@ -89,16 +92,59 @@ where
         self.height
     }
 
+    pub fn dimensions(&self) -> (usize, usize) {
+        (self.width, self.height)
+    }
+
+    pub fn expand_right(&mut self, amount: usize) {
+        for row in &mut self.data {
+            row.resize_with(row.len() + amount, T::default);
+        }
+        self.width += amount;
+    }
+
+    pub fn expand_bottom(&mut self, amount: usize) {
+        self.data
+            .resize_with(self.data.len() + amount, || vec![T::default(); self.width]);
+        self.height += amount;
+    }
+
+    pub fn expand_left(&mut self, amount: usize) {
+        for row in &mut self.data {
+            let mut new_row = vec![T::default(); amount];
+            new_row.append(row);
+            *row = new_row;
+        }
+
+        self.width += amount;
+        self.top_left.x -= amount as i64;
+    }
+
+    pub fn expand_top(&mut self, amount: usize) {
+        let mut new_data = vec![vec![T::default(); self.width]; amount];
+        new_data.append(&mut self.data);
+        self.data = new_data;
+
+        self.height += amount;
+        self.top_left.y -= amount as i64;
+    }
+
     pub fn is_within_bounds(&self, point: GridPoint) -> bool {
-        let GridPoint { x, y } = point;
-        x >= 0 && x < self.width as i64 && y >= 0 && y < self.height as i64
+        self.is_within_bounds_x(point.x) && self.is_within_bounds_y(point.y)
+    }
+
+    pub fn is_within_bounds_x(&self, x: i64) -> bool {
+        x >= self.top_left.x && x < self.top_left.x + self.width as i64
+    }
+
+    pub fn is_within_bounds_y(&self, y: i64) -> bool {
+        y >= self.top_left.y && y < self.top_left.y + self.height as i64
     }
 
     pub fn get(&self, point: GridPoint) -> Option<&T> {
         if self.is_within_bounds(point) {
-            self.data
-                .get(point.y as usize)
-                .and_then(|row| row.get(point.x as usize))
+            let ipoint = point.to_index_point(&self.top_left);
+            self.data.get(ipoint.y).and_then(|row| row.get(ipoint.x))
         } else {
             None
         }
@@ -106,9 +152,10 @@ where
 
     pub fn get_mut(&mut self, point: GridPoint) -> Option<&mut T> {
         if self.is_within_bounds(point) {
+            let ipoint = point.to_index_point(&self.top_left);
             self.data
-                .get_mut(point.y as usize)
-                .and_then(|row| row.get_mut(point.x as usize))
+                .get_mut(ipoint.y)
+                .and_then(|row| row.get_mut(ipoint.x))
         } else {
             None
         }
@@ -122,21 +169,51 @@ where
     ///
     /// I wouldn't recommend using this, but it's here anyways
     pub fn must_get(&self, point: GridPoint) -> &T {
-        &self.data[point.y as usize][point.x as usize]
+        let ipoint = point.to_index_point(&self.top_left);
+        &self.data[ipoint.y][ipoint.x]
     }
 
     pub fn set(&mut self, point: GridPoint, value: T) -> Result<(), String> {
         if self.is_within_bounds(point) {
-            self.data[point.y as usize][point.x as usize] = value;
+            let ipoint = point.to_index_point(&self.top_left);
+            self.data[ipoint.y][ipoint.x] = value;
             Ok(())
         } else {
             Err(format!("{} is out of bounds", point))
         }
     }
 
-    pub fn row(&self, y: usize) -> Option<&Vec<T>> {
-        if y < self.height {
-            Some(&self.data[y])
+    /// Set the value at (x, y), expanding the grid if necessary
+    ///
+    /// This still disallows setting negative coordinates
+    pub fn set_expand(&mut self, point: GridPoint, value: T) -> Result<(), String> {
+        // println!(
+        //     "set_expand({}, _), top_left = {}, dimensions = ({}, {})",
+        //     point, self.top_left, self.width, self.height
+        // );
+        if point.x >= self.top_left.x + self.width as i64 {
+            let amount = (point.x - (self.top_left.x + self.width as i64) + 1) as usize;
+            self.expand_right(amount);
+        } else if point.x < self.top_left.x {
+            let amount = (self.top_left.x - point.x) as usize;
+            self.expand_left(amount);
+        }
+
+        if point.y >= self.top_left.y + self.height as i64 {
+            let amount = (point.y - (self.top_left.y + self.height as i64) + 1) as usize;
+            self.expand_bottom(amount);
+        } else if point.y < self.top_left.y {
+            let amount = (self.top_left.y - point.y) as usize;
+            self.expand_top(amount);
+        }
+
+        self.set(point, value)
+    }
+
+    pub fn row(&self, y: i64) -> Option<&Vec<T>> {
+        if self.is_within_bounds_y(y) {
+            let iy = (y - self.top_left.y) as usize;
+            Some(&self.data[iy])
         } else {
             None
         }
@@ -146,9 +223,10 @@ where
     ///
     /// Due to the way the data is stored, an "artificial" column is created to hold the data
     /// which results in the column effectively being a cloned vector
-    pub fn build_column(&self, x: usize) -> Option<Vec<&T>> {
-        if x < self.width {
-            Some(self.data.iter().map(|row| &row[x]).collect())
+    pub fn build_column(&self, x: i64) -> Option<Vec<&T>> {
+        if self.is_within_bounds_x(x) {
+            let ix = (x - self.top_left.x) as usize;
+            Some(self.data.iter().map(|row| &row[ix]).collect())
         } else {
             None
         }
@@ -166,7 +244,7 @@ where
         (0..self.width).map(move |x| (0..self.height).map(move |y| &self.data[y][x]))
     }
 
-    pub fn insert_row_at(&mut self, y: usize, row: Vec<T>) -> Result<(), String> {
+    pub fn insert_row_at(&mut self, y: i64, row: Vec<T>) -> Result<(), String> {
         if row.len() != self.width {
             return Err(format!(
                 "Row must have {} elements, but has {}",
@@ -174,12 +252,17 @@ where
                 row.len()
             ));
         }
-        self.data.insert(y, row);
+        if !self.is_within_bounds_y(y as i64) {
+            return Err(format!("Row {} is out of bounds", y));
+        }
+
+        let iy = (y - self.top_left.y) as usize;
+        self.data.insert(iy, row);
         self.height += 1;
         Ok(())
     }
 
-    pub fn insert_column_at(&mut self, x: usize, mut column: Vec<T>) -> Result<(), String> {
+    pub fn insert_column_at(&mut self, x: i64, mut column: Vec<T>) -> Result<(), String> {
         if column.len() != self.height {
             return Err(format!(
                 "Column must have {} elements, but has {}",
@@ -187,21 +270,52 @@ where
                 column.len()
             ));
         }
+        if !self.is_within_bounds_x(x) {
+            return Err(format!("Column {} is out of bounds", x));
+        }
 
         for (y, element) in column.drain(..).enumerate() {
-            self.data[y].insert(x, element);
+            let ix = (x - self.top_left.x) as usize;
+            self.data[y].insert(ix, element);
         }
         self.width += 1;
         Ok(())
     }
 
+    pub fn flood<FV, FF>(
+        &mut self,
+        point: GridPoint,
+        value: &FV,
+        is_floodable: &FF,
+    ) -> Result<(), String>
+    where
+        FV: Fn() -> T,
+        FF: Fn(&T) -> bool,
+    {
+        // check if this location even makes sense
+        if !self.is_within_bounds(point) {
+            return Ok(());
+        }
+
+        // check if we've already flooded this location
+        if !is_floodable(self.must_get(point)) {
+            return Ok(());
+        }
+
+        self.set(point, value())?;
+
+        // flood the rest
+        for neighbor in point.orthogonal_neighbors() {
+            self.flood(neighbor, value, is_floodable)?;
+        }
+
+        Ok(())
+    }
+
     pub fn entries(&self) -> impl Iterator<Item = GridEntry<&T>> {
-        self.data.iter().enumerate().flat_map(|(y, row)| {
+        self.data.iter().enumerate().flat_map(move |(y, row)| {
             row.iter().enumerate().map(move |(x, value)| GridEntry {
-                point: GridPoint {
-                    x: x as i64,
-                    y: y as i64,
-                },
+                point: IndexPoint::from((x, y)).to_grid_point(&self.top_left),
                 value,
             })
         })
@@ -238,6 +352,7 @@ impl<T> TryFrom<Vec<Vec<T>>> for Grid<T> {
         }
 
         Ok(Self {
+            top_left: GridPoint { x: 0, y: 0 },
             data: value,
             width,
             height,
@@ -252,12 +367,40 @@ pub struct GridEntry<T> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct IndexPoint {
+    x: usize,
+    y: usize,
+}
+
+impl IndexPoint {
+    fn to_grid_point(&self, top_left: &GridPoint) -> GridPoint {
+        GridPoint {
+            x: (self.x as i64) + top_left.x,
+            y: (self.y as i64) + top_left.y,
+        }
+    }
+}
+
+impl From<(usize, usize)> for IndexPoint {
+    fn from((x, y): (usize, usize)) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct GridPoint {
     pub x: i64,
     pub y: i64,
 }
 
 impl GridPoint {
+    fn to_index_point(&self, top_left: &GridPoint) -> IndexPoint {
+        IndexPoint {
+            x: (self.x - top_left.x) as usize,
+            y: (self.y - top_left.y) as usize,
+        }
+    }
+
     pub fn direction_to_orthogonal(&self, other: &GridPoint) -> Result<CardinalDirection, String> {
         let x_diff = other.x - self.x;
         let y_diff = other.y - self.y;
@@ -371,8 +514,8 @@ impl std::fmt::Display for GridPoint {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum CardinalDirection {
     North,
-    East,
     South,
+    East,
     West,
 }
 
@@ -380,8 +523,8 @@ impl CardinalDirection {
     pub fn all() -> [Self; 4] {
         [
             CardinalDirection::North,
-            CardinalDirection::East,
             CardinalDirection::South,
+            CardinalDirection::East,
             CardinalDirection::West,
         ]
     }
@@ -420,6 +563,52 @@ impl CardinalDirection {
             CardinalDirection::West => CardinalDirection::North,
         }
     }
+
+    pub fn relative_direction_to(&self, other: &CardinalDirection) -> RelativeDirection {
+        match (self, other) {
+            (CardinalDirection::North, CardinalDirection::North) => RelativeDirection::Forward,
+            (CardinalDirection::North, CardinalDirection::South) => RelativeDirection::Backward,
+            (CardinalDirection::North, CardinalDirection::East) => RelativeDirection::Right,
+            (CardinalDirection::North, CardinalDirection::West) => RelativeDirection::Left,
+            (CardinalDirection::South, CardinalDirection::North) => RelativeDirection::Backward,
+            (CardinalDirection::South, CardinalDirection::South) => RelativeDirection::Forward,
+            (CardinalDirection::South, CardinalDirection::East) => RelativeDirection::Left,
+            (CardinalDirection::South, CardinalDirection::West) => RelativeDirection::Right,
+            (CardinalDirection::East, CardinalDirection::North) => RelativeDirection::Left,
+            (CardinalDirection::East, CardinalDirection::South) => RelativeDirection::Right,
+            (CardinalDirection::East, CardinalDirection::East) => RelativeDirection::Forward,
+            (CardinalDirection::East, CardinalDirection::West) => RelativeDirection::Backward,
+            (CardinalDirection::West, CardinalDirection::North) => RelativeDirection::Right,
+            (CardinalDirection::West, CardinalDirection::South) => RelativeDirection::Left,
+            (CardinalDirection::West, CardinalDirection::East) => RelativeDirection::Backward,
+            (CardinalDirection::West, CardinalDirection::West) => RelativeDirection::Forward,
+        }
+    }
+
+    pub fn halfway_to(&self, other: &CardinalDirection) -> Result<OrdinalDirection, String> {
+        match (self, other) {
+            (CardinalDirection::North, CardinalDirection::East)
+            | (CardinalDirection::East, CardinalDirection::North) => {
+                Ok(OrdinalDirection::NorthEast)
+            }
+            (CardinalDirection::South, CardinalDirection::East)
+            | (CardinalDirection::East, CardinalDirection::South) => {
+                Ok(OrdinalDirection::SouthEast)
+            }
+            (CardinalDirection::South, CardinalDirection::West)
+            | (CardinalDirection::West, CardinalDirection::South) => {
+                Ok(OrdinalDirection::SouthWest)
+            }
+            (CardinalDirection::North, CardinalDirection::West)
+            | (CardinalDirection::West, CardinalDirection::North) => {
+                Ok(OrdinalDirection::NorthWest)
+            }
+            _ => Err(format!(
+                "cardinal directions {} and {} are opposite",
+                self, other
+            )),
+        }
+    }
 }
 
 impl From<CardinalDirection> for char {
@@ -433,10 +622,72 @@ impl From<CardinalDirection> for char {
     }
 }
 
+impl std::fmt::Display for CardinalDirection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CardinalDirection::North => write!(f, "north"),
+            CardinalDirection::South => write!(f, "south"),
+            CardinalDirection::East => write!(f, "east"),
+            CardinalDirection::West => write!(f, "west"),
+        }
+    }
+}
+
+pub enum OrdinalDirection {
+    NorthEast,
+    SouthEast,
+    SouthWest,
+    NorthWest,
+}
+
+impl OrdinalDirection {
+    pub fn all() -> [Self; 4] {
+        [
+            OrdinalDirection::NorthEast,
+            OrdinalDirection::SouthEast,
+            OrdinalDirection::SouthWest,
+            OrdinalDirection::NorthWest,
+        ]
+    }
+
+    pub fn opposite(&self) -> Self {
+        match self {
+            OrdinalDirection::NorthEast => OrdinalDirection::SouthWest,
+            OrdinalDirection::SouthEast => OrdinalDirection::NorthWest,
+            OrdinalDirection::SouthWest => OrdinalDirection::NorthEast,
+            OrdinalDirection::NorthWest => OrdinalDirection::SouthEast,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RelativeDirection {
     Forward,
     Backward,
     Left,
     Right,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Winding {
+    Left,
+    Right,
+}
+
+impl From<Winding> for RelativeDirection {
+    fn from(value: Winding) -> Self {
+        match value {
+            Winding::Left => RelativeDirection::Left,
+            Winding::Right => RelativeDirection::Right,
+        }
+    }
+}
+
+impl std::fmt::Display for Winding {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Winding::Left => write!(f, "left"),
+            Winding::Right => write!(f, "right"),
+        }
+    }
 }
